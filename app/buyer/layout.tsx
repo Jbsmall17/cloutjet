d772d9930c5d4c2b4a192f56e1f37a7bbc755f9d
Ajibola <alaomiyaki17@gmail.com>
2025-07-10 10:17:43 +0100

    fixed ui error

--- app/buyer/layout.tsx
+++ app/buyer/layout.tsx
@@ -6,12 +6,12 @@ import React from 'react'
 export default function Layout({children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = React.useState(false)
   return (
-    <div className='h-screen overflow-hidden bg-[#f4f4f4] flex flex-col'>
+    <div className='h-auto overflow-hidden bg-[#f4f4f4] flex flex-col'>
         <BuyerHeader 
           isOpen={isOpen} 
           setIsOpen={setIsOpen}
         />
-        <main className='relative flex flex-col lg:flex-row gap-4 md:gap-6 h-auto md:h-[85vh]'>
+        <main className='relative flex flex-col lg:flex-row gap-4 md:gap-6 overflow-auto maincontent'>
           <BuyerSidebar 
             isOpen={isOpen}
           />
